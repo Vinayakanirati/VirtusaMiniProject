93eb46e160a43a04786456410e74fefe7b3c99db
vinayaka <=>
2026-04-09 10:46:48 +0530

    added .sql file

--- LibraryAudit.sql
+++ LibraryAudit.sql
@@ -1,6 +1,4 @@
-**DIGITAL LIBRARY**
-*ddl commands*
--- Create Books Table
+
 CREATE TABLE Books (
     BookID INT PRIMARY KEY,
     Title VARCHAR(100),
@@ -9,7 +7,6 @@ CREATE TABLE Books (
     AvailableCopies INT
 );
 
--- Create Students Table
 CREATE TABLE Students (
     StudentID INT PRIMARY KEY,
     Name VARCHAR(100),
@@ -17,7 +14,7 @@ CREATE TABLE Students (
     JoinDate DATE
 );
 
--- Create IssuedBooks Table
+
 CREATE TABLE IssuedBooks (
     IssueID INT PRIMARY KEY,
     BookID INT,
@@ -28,7 +25,7 @@ CREATE TABLE IssuedBooks (
     FOREIGN KEY (StudentID) REFERENCES Students(StudentID)
 );
 
-**Students with overdue**
+
 
 select s.StudentID , s.Name , b.title,ib.issuedDate, DATEDIFF(CURDATE(),ib.IssueDtae) AS DaysOverdue
 from issuedBooks ib join
@@ -37,7 +34,6 @@ join Books b on b.bookID =ib.BookID where
 ib.ReturnDate IS null and 
 DATEDIFF(CURDATE(),ib.IssuedDate)>14;
 
-**popularity index**
 SELECT
     b.Category,
      COUNT(*) AS TotalBorrows
@@ -46,7 +42,6 @@ SELECT
      GROUP BY b.Category
      ORDER BY TotalBorrows DESC;
 
-**INACTIVE USERS**
 
 ALTER TABLE Students ADD Status VARCHAR(20) DEFAULT 'Active';
 
